f7c4ce7da56ef380dd40afcab90d2860e0363f75
2021cfse031 <94062868+2021cfse031@users.noreply.github.com>
2022-07-02 22:12:44 +0530

    update sonarrqube

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -8,7 +8,6 @@ scanner(
     "sonar.sources": "./src",
     "sonar.projectKey": "app",
     "sonar.login":"5a4d85805e32dc64a1c81b81b2812c96c92a5e3a",
-    "sonar.exclusions": "**/*.test.js",
     "sonar.tests": "./src",
     "sonar.test.inclusions": "App.test.js",
     "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",

dbe5cb99b7b71d97ba284aaf1de32e3c378eb208
2021cfse031 <94062868+2021cfse031@users.noreply.github.com>
2022-07-02 22:38:12 +0530

    update sonarrqube

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -6,7 +6,7 @@ scanner(
   password:"",
   options: {
     "sonar.sources": "./src",
-    "sonar.projectKey": "app",
+    "sonar.projectKey": "app1",
     "sonar.login":"5a4d85805e32dc64a1c81b81b2812c96c92a5e3a",
     "sonar.exclusions": "**/*.test.js",
     "sonar.tests": "./src",
